69943be7b9a104b3ab13620555f4c82c37df75f5
Hanna <134929928+likevelerjegher@users.noreply.github.com>
2025-04-24 14:39:40 +0300

    Add files via upload

--- part1.6/main.py
+++ part1.6/main.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import math
 
-link = "http://suninjuly.github.io/simple_form_find_task.html"
+link = "http://suninjuly.github.io/find_link_text"
 
 try:
     browser = webdriver.Chrome()
     browser.get(link)
+    link = browser.find_element(By.LINK_TEXT, str(math.ceil(math.pow(math.pi, math.e)*10000)))
+
+    link.click()
 
     input1 = browser.find_element(By.TAG_NAME, "input")
     input1.send_keys("Ivan")
@@ -18,6 +22,7 @@ try:
     input4.send_keys("Russia")
     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
+
 finally:
     time.sleep(30)
     browser.quit()
